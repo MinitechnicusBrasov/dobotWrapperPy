ff497026664a5ae7542428d61849c183ce603505
andreishark <andreishark10@gmail.com>
2025-05-20 17:17:35 +0300

    Setup version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# mypy: disable-error-code="import"
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -6,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="dobotWrapperPy",
     packages=["dobotWrapperPy", "dobotWrapperPy.enums"],
-    version="0.0.1",
+    version="0.1.0",
     description="Python library for Dobot Magician For Minitechnicus Courses",
     long_description=long_description,
     long_description_content_type="text/markdown",
