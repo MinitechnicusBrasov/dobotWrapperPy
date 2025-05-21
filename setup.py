f8d8d3debd7e64ab3118bd0376456c51eed001e4
andreishark <56549903+andreishark@users.noreply.github.com>
2025-05-21 15:35:51 +0300

    Update to v0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     packages=["dobotWrapperPy", "dobotWrapperPy.enums"],
     package_data={"dobotWrapperPy": ["py.typed"], "dobotWrapperPy.enums": ["py.typed"]},
     include_package_data=True,
-    version="0.1.5",
+    version="0.1.6",
     description="Python library for Dobot Magician For Minitechnicus Courses",
     long_description=long_description,
     long_description_content_type="text/markdown",
