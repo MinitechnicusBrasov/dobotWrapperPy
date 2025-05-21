e0b97d8e9bf6f0b560583cbbff38084894026ada
andreishark <andreishark10@gmail.com>
2025-05-21 16:21:57 +0300

    Fix example

--- basic_example.py
+++ basic_example.py
@@ -1,12 +1,12 @@
 from serial.tools import list_ports
 
-import pydobot
+import dobotWrapperPy
 
 available_ports = list_ports.comports()
 print(f"available ports: {[x.device for x in available_ports]}")
 port = available_ports[0].device
 
-device = pydobot.Dobot(port=port, verbose=True)
+device = dobotWrapperPy.Dobot(port=port, verbose=True)
 
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
 print(f"x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}")
@@ -16,5 +16,3 @@ for i in range(2):
     device.wait(1000)
     device.move_to(x, y, z, r, wait=True)
     device.wait(1000)
-
-device.close()
